d497b1a13d1b642be9bd39f9ab372caecbef54b1
Emerson Sousa <eyenessus@gmail.com>
2022-09-03 16:45:54 -0300

    add

--- App.js
+++ App.js
@@ -1,57 +1,70 @@
-import React, {Component} from 'react'
-  import {View, StyleSheet, TouchableOpacity, Text} from 'react-native'
-  
-  //telas
-  import Home from './src/Pages/Home'
-  import Redirect from './src/Pages/Redirect'
+import React, {Component} from 'react';
+import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
 
-  //notificacao
-  import { NotificationManager } from './src/Notification'
-  
-  //navegacao
-   import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-  
+//telas
+import Home from './src/Pages/Home';
+import Redirect from './src/Pages/Redirect';
+
+//notificacao
+import {notificationManager} from './src/notification';
+
+//navegacao
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 //declaracao de constantes
 
-const localNotify = NotificationManager;
+const localNotify = notificationManager;
 const Stack = createStackNavigator();
 
-  export default class App extends Component {
-    constructor(props) {
-      super(props)
-    }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    
-    componentDidMount() {
-      localNotify.configure()
-      localNotify.creatChannel()  
-      localNotify.construirnotificacaoagendada();
-    }
 
-    onPressSendNotification = () => {
-      localNotify.showNotification(
-        1,
-        "Esse é o nosso título",
-        "E aqui está a mensagem. Vamos inserir uma mensagem um pouco mais longa para vermos o Android irá se adaptar ao conteúdo na tela?",
-        {}, // data
-        {} // options
-      )
-    }
+  componentDidMount() {
+    localNotify.configure();
+    localNotify.createChannel();
+    localNotify.buildNotificationSchedule();
+  }
 
-    onPressCancelAllLocalNotification = () => {
-      localNotify.cancelAllLocalNotification()
-    }
+  onPressSendNotification = () => {
+    localNotify.showNotification(
+      1,
+      "Esse é o nosso título",
+      "E aqui está a mensagem. Vamos inserir uma mensagem um pouco mais longa para vermos o Android irá se adaptar ao conteúdo na tela?",
+      {}, // data
+      {} // options
+    )
+  }
 
-    render() {
-      return(
+  onPressCancelAllLocalNotification = () => {
+    localNotify.cancelAllLocalNotification()
+  }
+
+  render() {
+    return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Pagina princial" component={Home}/>
-          <Stack.Screen name="Redirect" component={Redirect}/>
+
+          <Stack.Screen name="Home">
+          {
+            ({navigation}) => {
+              localNotify.setNavegador(navigation)
+              return (
+                <Home 
+                MandarNotificacao={this.onPressSendNotification}
+                CancelarNotificacao={this.onPressCancelAllLocalNotification}
+                />
+              )
+            }
+          }
+        
+            </Stack.Screen>   
+
+          <Stack.Screen name="Redirect" component={Redirect} />
         </Stack.Navigator>
       </NavigationContainer>
-      )
-      
-    }
+    );
   }
+}
